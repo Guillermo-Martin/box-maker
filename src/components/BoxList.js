e48d933b720033c3130d7e867dc95178c70cc1b8
gcmartin <gscalica@gmail.com>
2020-08-18 14:10:29 -0700

    Create addInfo function in BoxList component and connect with NewBoxForm component

--- src/components/BoxList.js
+++ src/components/BoxList.js
@@ -3,10 +3,20 @@ import Box from './Box';
 import NewBoxForm from './NewBoxForm';
 
 class BoxList extends Component {
+
+// create method here that will add info from the form 
+// it will take the info from form as an argument
+// this info will be an object
+// method will update state; use callback form
+
+  addInfo = () => {
+    alert("I'm connected!")
+  }
+
   render() {
     return (
       <div>
-        <NewBoxForm />
+        <NewBoxForm addInfo={this.addInfo} />
         <Box />
 
       </div>
--- src/components/NewBoxForm.js
+++ src/components/NewBoxForm.js
@@ -13,7 +13,8 @@ class NewBoxForm extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    alert(`Height: ${this.state.height}, width: ${this.state.width}, color: ${this.state.color}`)
+    // alert(`Height: ${this.state.height}, width: ${this.state.width}, color: ${this.state.color}`)
+    this.props.addInfo();
   }
 
   render() {
